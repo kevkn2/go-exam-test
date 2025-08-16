344799fa9c8008c1d8c2b906b7f18b2f2d4676ca
kevkn2 <jimmyjuan3@gmail.com>
2025-08-16 09:42:36 +0700

    feat: set create user and student in 1 transaction

--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5/pgconn"
+	"gorm.io/gorm"
 )
 
 type AuthService interface {
@@ -24,6 +25,7 @@ type AuthService interface {
 type authService struct {
 	userRepo    repositories.UserRepository
 	studentRepo repositories.StudentRepository
+	db          *gorm.DB
 }
 
 // CreateStudent implements UserService.
@@ -31,45 +33,56 @@ func (u *authService) CreateStudent(reg schemas.RegisterStudentRequestSchema) (s
 	*models.User
 	*models.Student
 }, error) {
-	user := models.User{
-		Email:     reg.Email,
-		Password:  reg.Password,
-		Authority: "student",
-	}
-
-	resultUser, err := u.userRepo.CreateUser(&user)
-
-	var pgErr *pgconn.PgError
-
-	if errors.As(err, &pgErr) {
-		if pgErr.Code == "23505" {
-			return struct {
-				*models.User
-				*models.Student
-			}{}, fmt.Errorf("student already exists")
-		}
-	}
-
-	student := models.Student{
-		Name:   reg.Name,
-		School: reg.School,
-		UserID: resultUser.ID,
-	}
-
-	resultStudent, err := u.studentRepo.CreateStudent(student)
-	if errors.As(err, &pgErr) {
-		if pgErr.Code == "23505" {
-			return struct {
-				*models.User
-				*models.Student
-			}{}, fmt.Errorf("student already exists")
-		}
-	}
-
-	return struct {
+	var result struct {
 		*models.User
 		*models.Student
-	}{resultUser, resultStudent}, nil
+	}
+
+	err := u.db.Transaction(func(tx *gorm.DB) error {
+		user := models.User{
+			Email:     reg.Email,
+			Password:  reg.Password,
+			Authority: "student",
+		}
+
+		resultUser, err := u.userRepo.WithTx(tx).CreateUser(&user)
+		if err != nil {
+			var pgErr *pgconn.PgError
+			if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+				return fmt.Errorf("student already exists")
+			}
+			return err
+		}
+
+		student := models.Student{
+			Name:   reg.Name,
+			School: reg.School,
+			UserID: resultUser.ID,
+		}
+
+		resultStudent, err := u.studentRepo.WithTx(tx).CreateStudent(student)
+		if err != nil {
+			var pgErr *pgconn.PgError
+			if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+				return fmt.Errorf("student already exists")
+			}
+			return err
+		}
+
+		// store results for returning after transaction
+		result.User = resultUser
+		result.Student = resultStudent
+		return nil
+	})
+
+	if err != nil {
+		return struct {
+			*models.User
+			*models.Student
+		}{}, err
+	}
+
+	return result, nil
 }
 
 // CreateAdmin implements UserService.
@@ -112,9 +125,11 @@ func (u *authService) AuthorizeStudent(id uint) (*models.User, error) {
 func NewAuthService(
 	userRepo repositories.UserRepository,
 	studentRepo repositories.StudentRepository,
+	db *gorm.DB,
 ) AuthService {
 	return &authService{
 		userRepo:    userRepo,
 		studentRepo: studentRepo,
+		db:          db,
 	}
 }
